6fc7c3eeb4c90a0e21d326f9a0f2d5180630c713
Sebastian Kornehl <sebastian.kornehl@codecentric.de>
2018-11-26 11:37:08 +0100

    corrected wrong order within cleanup

--- template/md/network/example.sh
+++ template/md/network/example.sh
@@ -13,7 +13,7 @@ docker run -it --rm --network mytestnet2 alpine \
     bin/sh -c 'apk add curl; curl web1'
 
 # Cleanup
-docker network rm mytestnet1
-docker network rm mytestnet2
 docker stop web1
 docker rm web1
+docker network rm mytestnet1
+docker network rm mytestnet2
